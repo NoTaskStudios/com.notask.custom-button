3e98ca9f67968c7b3d6226758aade8aa4d668828
Leniton <lenitoncarneiro@gmail.com>
2024-10-29 16:40:29 -0300

    feat: interactable and color enable

--- Assets/Package/Runtime/Editor/CustomButtonEditor.cs
+++ Assets/Package/Runtime/Editor/CustomButtonEditor.cs
@@ -1,6 +1,8 @@
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 namespace CustomButton
 {
     [CanEditMultipleObjects]
@@ -8,13 +10,15 @@ namespace CustomButton
     public class CustomButtonEditor : Editor
     {
         private Texture2D customIcon;
+        private Texture2D circleIcon;
         private SerializedProperty interactableProperty;
+        private SerializedProperty targetGraphicProperty;
+        private int selectedTab;
         // General
         private SerializedProperty opacityOnChildrenProperty;
         private SerializedProperty offsetOnChildrenProperty;
         // Color Tint
         private SerializedProperty invertTextColorProperty;
-        private SerializedProperty targetGraphicProperty;
         private SerializedProperty blockColorsProperty;
         private Color previousNormalColor;
         // Sprite Swap
@@ -40,7 +44,11 @@ namespace CustomButton
         private bool currentInteractable;
         
         private const string SelectedTabPreferenceKeyPrefix = "SelectedTabPreferenceKey_";
-        private int selectedTab;
+
+        //Cache
+        private CustomButtonBase customButton;
+        private UnityEngine.UI.Graphic currentGraphic;
+
         private void OnEnable()
         {
             LoadIconResource();
@@ -69,8 +77,6 @@ namespace CustomButton
             activeColorTintProperty = serializedObject.FindProperty("activeColorTint");
             activeSpriteSwapProperty = serializedObject.FindProperty(nameof(CustomButtonBase.activeSpriteSwap));
             activeAnimationProperty = serializedObject.FindProperty("activeAnimation");
-            selectedTab = EditorPrefs.GetInt(GetGameObjectKey(target), 0);
-            SelectTab(selectedTab);
         }
         private void LoadIconResource()
         {
@@ -78,160 +84,9 @@ namespace CustomButton
             var sprite = Resources.Load<Texture2D>(assetPath);
             customIcon = sprite;
         }
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
-            var customButtonBase = (CustomButtonBase)target;
-            EditorGUIUtility.SetIconForObject(target, customIcon);
-            EditorGUI.BeginChangeCheck();
-            var interactableValue = EditorGUILayout.Toggle("Interactable", interactableProperty.boolValue);
-            if (EditorGUI.EndChangeCheck())
-            {
-                ((CustomButtonClass)target).Interactable = interactableValue;
-                interactableProperty.boolValue = interactableValue;
-                interactableProperty.serializedObject.ApplyModifiedProperties();
-                SceneView.RepaintAll();
-                EditorUtility.SetDirty(customButtonBase);
-            }
-            string[] tabs = { "General", "Color Tint", "Sprite Swap", "Animation" };
-            selectedTab = GUILayout.Toolbar(selectedTab, tabs);
-            if (selectedTab != EditorPrefs.GetInt(GetGameObjectKey(target), 0))
-            {
-                EditorPrefs.SetInt(GetGameObjectKey(target), selectedTab);
-                SelectTab(selectedTab);
-            }
-            EditorGUILayout.Space();
-            if (showGeneralProperties)
-            {
-                EditorGUILayout.PropertyField(targetGraphicProperty);
-                const int size = 16;
-                var colorTintLabel = new GUIContent("Status Color Tint:");
-                var spriteSwapLabel = new GUIContent("Status Sprite Swap:");
-                var animationLabel = new GUIContent("Status Animation Preset:");
-                var labelStyle = GUI.skin.label;
-                var calcColortint = labelStyle.CalcSize(colorTintLabel).x;
-                var calcSpriteSwap = labelStyle.CalcSize(spriteSwapLabel).x;
-                var calcAnimation = labelStyle.CalcSize(animationLabel).x;
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField(colorTintLabel , GUILayout.Width(calcColortint)); // Define a largura baseada no tamanho do rótulo
-                GUILayout.Label(CreateRoundTexture(size, SetColorStatus(customButtonBase.activeColorTint)), GUILayout.Width(size), GUILayout.Height(size));
-                EditorGUILayout.EndHorizontal();
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField(spriteSwapLabel , GUILayout.Width(calcSpriteSwap)); // Define a largura baseada no tamanho do rótulo
-                GUILayout.Label(CreateRoundTexture(size, SetColorStatus(customButtonBase.activeSpriteSwap)), GUILayout.Width(size), GUILayout.Height(size));
-                EditorGUILayout.EndHorizontal();
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField(animationLabel , GUILayout.Width(calcAnimation)); // Define a largura baseada no tamanho do rótulo
-                GUILayout.Label(CreateRoundTexture(size, SetColorStatus(customButtonBase.activeAnimation)), GUILayout.Width(size), GUILayout.Height(size));
-                EditorGUILayout.EndHorizontal();
-            }
-            if (showColorProperties)
-            {
-                var activeColor = EditorGUILayout.Toggle("Active Color Tint", activeColorTintProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    activeColorTintProperty.boolValue = activeColor;
-                    activeColorTintProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUI.indentLevel++;
-                EditorGUI.BeginChangeCheck();
-                var invertColorText = EditorGUILayout.Toggle("Invert Color Text", invertTextColorProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    invertTextColorProperty.boolValue = invertColorText;
-                    invertTextColorProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUI.BeginChangeCheck();
-                var opacityChildrenText = EditorGUILayout.Toggle("Apply Opacity Children", opacityOnChildrenProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    opacityOnChildrenProperty.boolValue = opacityChildrenText;
-                    opacityOnChildrenProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUILayout.PropertyField(blockColorsProperty);
-                EditorGUI.indentLevel--;
-                // Só aplicar esta fução caso mexa no colorpicker no inpector
-                //customButtonBase.UpdateColor(customButtonBase.blockColors.normalColor);
-                
-                EditorUtility.SetDirty(customButtonBase);
-            }
-            
-            if (showSpriteProperties)
-            {
-                var activeSprite = EditorGUILayout.Toggle("Active Sprite Swap", activeSpriteSwapProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    activeSpriteSwapProperty.boolValue = activeSprite;
-                    activeSpriteSwapProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUI.indentLevel++;
-                EditorGUI.BeginChangeCheck();
-                var offsetChildren = EditorGUILayout.Toggle("Apply Offset Children", offsetOnChildrenProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    offsetOnChildrenProperty.boolValue = offsetChildren;
-                    offsetOnChildrenProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUI.BeginChangeCheck();
-                var activeBlink = EditorGUILayout.Toggle("Apply Blink Highlighted", applyBlinkHighlightedProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    applyBlinkHighlightedProperty.boolValue = activeBlink;
-                    applyBlinkHighlightedProperty.serializedObject.ApplyModifiedProperties();
-                }
-                if (offsetChildren)
-                {
-                    EditorGUI.indentLevel++;
-                    EditorGUILayout.PropertyField(offsetVectorChildrenProperty);
-                    EditorGUI.indentLevel--;
-                }
-                EditorGUILayout.PropertyField(spriteSwapProperty, true);
-                EditorGUI.indentLevel--;
-            }
-            if (showAnimationProperties)
-            {
-                var activeAnimation = EditorGUILayout.Toggle("Active Animation Preset", activeAnimationProperty.boolValue);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    activeAnimationProperty.boolValue = activeAnimation;
-                    activeAnimationProperty.serializedObject.ApplyModifiedProperties();
-                }
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(normalAnimProperty, true);
-                EditorGUILayout.PropertyField(highlightedAnimProperty, true);
-                EditorGUILayout.PropertyField(pressedAnimProperty, true);
-                EditorGUILayout.PropertyField(selectedAnimProperty, true);
-                EditorGUILayout.PropertyField(disabledAnimProperty, true);
-                EditorGUI.indentLevel--;
-            }
-            EditorGUILayout.Space();
-            EditorGUILayout.PropertyField(onClickProperty);
-            serializedObject.ApplyModifiedProperties();
-        }
 
         private static Color SetColorStatus(bool isActive) => isActive ? Color.green : Color.red;
-        private static string GetGameObjectKey(Object targetObject) => SelectedTabPreferenceKeyPrefix + targetObject.name + "_" + targetObject.GetType().Name;
-        private void SelectTab(int tab)
-        {
-            ResetTabs();
-            switch (tab)
-            {
-                case 0: // General
-                    showGeneralProperties = true;
-                    break;
-                case 1: // Color Tint
-                    showColorProperties = true;
-                    break;
-                case 2: // Sprite Swap
-                    showSpriteProperties = true;
-                    break;
-                case 3: // Animation
-                    showAnimationProperties = true;
-                    break;
-            }
-            serializedObject.ApplyModifiedProperties();
-        }
-        private static Texture2D CreateRoundTexture(int size, Color color)
+        private Texture2D CreateRoundTexture(int size, Color color)
         {
             var texture = new Texture2D(size, size);
             var pixels = new Color[size * size];
@@ -260,15 +115,67 @@ namespace CustomButton
 
             texture.SetPixels(pixels);
             texture.Apply();
-
+            circleIcon = texture;
             return texture;
         }
-        private void ResetTabs()
+
+        public override VisualElement CreateInspectorGUI()
         {
-            showGeneralProperties = false;
-            showColorProperties = false;
-            showSpriteProperties = false;
-            showAnimationProperties = false;
+            #region Setup
+            serializedObject.Update();
+            customButton = target as CustomButtonBase;
+            EditorGUIUtility.SetIconForObject(target, customIcon);
+            #endregion
+
+            VisualElement root = new();
+
+            PropertyField interactable = new PropertyField(interactableProperty);
+            interactable.RegisterValueChangeCallback((evt) => customButton.UpdateButtonState());
+            root.Add(interactable);
+
+            PropertyField targetGraphic = new PropertyField(targetGraphicProperty);
+            currentGraphic = (UnityEngine.UI.Graphic)targetGraphicProperty.objectReferenceValue;
+            targetGraphic.RegisterValueChangeCallback((evt) =>
+            {
+                if (currentGraphic)
+                {
+                    currentGraphic.CrossFadeColor(Color.white, 0, true, true);
+                    if(currentGraphic as UnityEngine.UI.Image)
+                    {
+                        UnityEngine.UI.Image img = (UnityEngine.UI.Image)currentGraphic;
+                        img.overrideSprite = null;
+                    }
+                }
+                currentGraphic = (UnityEngine.UI.Graphic)evt.changedProperty.objectReferenceValue;
+                customButton.UpdateButtonState();
+            });
+            root.Add(targetGraphic);
+
+            VisualElement tabWindow = new VisualElement();
+            root.Add(tabWindow);
+
+            //Button GeneralTab
+            tabWindow.Add(TabButton("Color Tint", activeColorTintProperty));
+
+            return root;
+        }
+
+        private Button TabButton(string label,SerializedProperty enableProperty)
+        {
+            Button button = new Button();
+            button.text = label;
+            Toggle enableToggle = new Toggle();
+            enableToggle.value = enableProperty.boolValue;
+            enableToggle.style.width = 14;
+            enableToggle.RegisterValueChangedCallback((evt) =>
+            {
+                enableProperty.boolValue = evt.newValue;
+                serializedObject.ApplyModifiedProperties();
+                customButton.UpdateButtonState();
+            });
+            button.Add(enableToggle);
+
+            return button;
         }
     }
 }
